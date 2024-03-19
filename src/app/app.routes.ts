079d0849593ab56ce9823670b76e02e53a87e42d
Carlos <hola@cafeconcarlos.com>
2024-03-19 12:42:01 +0100

    detalle component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ListaProductosComponent } from './components/lista-productos/lista-productos.component';
+import { DetalleProductoComponent } from './components/detalle-producto/detalle-producto.component';
 
 export const routes: Routes = [
-    { path: 'productos', component: ListaProductosComponent }
+    { path: 'productos', component: ListaProductosComponent },
+    { path: 'productos/detalle', component: DetalleProductoComponent }
 ];
