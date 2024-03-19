4ab20baedd2cbd7813d4bf27bb593f206559f8c7
Mario Girón <mariogironm@gmail.com>
2024-03-19 12:44:44 +0100

    Creación formulario nuevo producto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ListaProductosComponent } from './components/lista-productos/lista-productos.component';
+import { NuevoProductoComponent } from './components/nuevo-producto/nuevo-producto.component';
 
 export const routes: Routes = [
-    { path: 'productos', component: ListaProductosComponent }
+    { path: 'productos', component: ListaProductosComponent },
+    { path: 'nuevo', component: NuevoProductoComponent }
 ];
