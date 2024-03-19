d0bc8721f5569f0aa70172e0eb0d3da0d86683f0
Mario Girón <mariogironm@gmail.com>
2024-03-19 13:08:10 +0100

    Cosas para la edición

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ListaProductosComponent } from './components/lista-productos/lista-productos.component';
 import { NuevoProductoComponent } from './components/nuevo-producto/nuevo-producto.component';
+import { EditProductoComponent } from './components/edit-producto/edit-producto.component';
 
 export const routes: Routes = [
     { path: 'productos', component: ListaProductosComponent },
-    { path: 'nuevo', component: NuevoProductoComponent }
+    { path: 'nuevo', component: NuevoProductoComponent },
+    { path: 'edit/:productoId', component: EditProductoComponent }
 ];
---
+++ src/app/components/edit-producto/edit-producto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-producto',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-producto.component.html',
+  styleUrl: './edit-producto.component.css'
+})
+export class EditProductoComponent {
+
+}
