353578de223cb485f062189d346af729d1873c36
Carlos <hola@cafeconcarlos.com>
2024-03-19 12:30:31 +0100

    fichero routas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ListaProductosComponent } from './components/lista-productos/lista-productos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'productos', component: ListaProductosComponent }
+];
